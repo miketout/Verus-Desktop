2ff9e05a8b869792ae3e4424d36969a8fd58e0d7
michaeltout <toutonghi.michael@gmail.com>
2024-04-12 16:37:50 +0200

    Refactor code to use OS tools to check an extra time if any daemon is running before closing the app

--- routes/api/utils/constants/index.js
+++ routes/api/utils/constants/index.js
@@ -1,7 +1,9 @@
 const allowed_paths = require('./allowed_paths')
 const file_descriptors = require('./file_descriptors')
+const daemons = require('./daemons')
 
 module.exports = {
   ...allowed_paths,
-  ...file_descriptors
+  ...file_descriptors,
+  ...daemons
 }
