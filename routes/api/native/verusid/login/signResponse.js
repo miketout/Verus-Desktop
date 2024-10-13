2b4f4b1c6cca4fdb67ea2744108c80b7b343f9b9
mcstoer <49734282+mcstoer@users.noreply.github.com>
2024-10-13 11:01:00 -0700

    Update handling of the deeplinks for login consent requests to match the newer version.

--- routes/api/native/verusid/login/signResponse.js
+++ routes/api/native/verusid/login/signResponse.js
@@ -4,6 +4,15 @@ const {
   LOGIN_CONSENT_RESPONSE_SIG_VDXF_KEY,
 } = require("verus-typescript-primitives");
 
+const {
+  ECPair,
+  networks,
+  IdentitySignature
+} = require("@bitgo/utxo-lib");
+
+const ID_SIG_VERSION = 2
+const ID_SIG_TYPE = 5
+
 module.exports = (api) => {
   api.native.verusid.login.sign_response = async (response) => {
     const loginResponse = new LoginConsentResponse(response);
@@ -16,22 +25,37 @@ module.exports = (api) => {
       throw new Error(verificatonCheck.message);
     }
 
-    const subject = loginResponse.decision.request.challenge.subject;
-    if (subject != null && loginResponse.signing_id !== subject)
-      throw new Error("Cannot sign request for different user.");
+    // Sign the decision hash of the response not using the daemon since sign_hash is disabled.
+    const info = await api.native.get_info(response.chain_id)
 
-    const userSignature = await api.native.sign_message(
-      loginResponse.chain_id,
-      loginResponse.signing_id,
-      loginResponse.getSignedData()
+    const signingIdentity = await api.native.get_identity(response.chain_id, loginResponse.signing_id);
+
+    const network = response.chain_id == "VRSC" ? networks.verus : networks.verustest;
+
+    const wif = await api.native.get_privkey(response.chain_id, signingIdentity.identity.primaryaddresses[0])
+
+    const keyPair = ECPair.fromWIF(wif, network);
+
+    const sig = new IdentitySignature(
+      network,
+      ID_SIG_VERSION,
+      ID_SIG_TYPE,
+      info.longestchain,
+      null,
+      response.system_id,
+      response.signing_id
     );
 
+    sig.signHashOffline(loginResponse.getDecisionHash(info.longestchain), keyPair);
+
+    const signedSig = sig.toBuffer().toString("base64");
+
     loginResponse.signature = new VerusIDSignature(
-      { signature: userSignature.signature },
+      { signature: signedSig },
       LOGIN_CONSENT_RESPONSE_SIG_VDXF_KEY
     );
 
-    return { response: loginResponse.stringable() };
+    return { response: loginResponse};
   };
 
   api.setPost("/native/verusid/login/sign_response", async (req, res, next) => {
--- routes/api/native/verusid/login/verifyRequest.js
+++ routes/api/native/verusid/login/verifyRequest.js
@@ -8,10 +8,23 @@ module.exports = (api) => {
   api.native.verusid.login.verify_request = async (request) => {
     const loginConsentRequest = new LoginConsentRequest(request);
 
-    const verified = await api.native.verify_message(
-      loginConsentRequest.chain_id,
+    let chain_id
+
+    switch (loginConsentRequest.system_id) {
+      case "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV":
+        chain_id = "VRSC";
+        break;
+      case "iJhCezBExJHvtyH3fGhNnt2NhU4Ztkf2yq":
+        chain_id = "VRSCTEST";
+        break;
+      default:
+        throw new Error("Unknown system id.")
+    }
+
+    const verified = await api.native.verify_hash(
+      chain_id,
       loginConsentRequest.signing_id,
-      loginConsentRequest.getSignedData(),
+      loginConsentRequest.challenge.toSha256().toString('hex'),
       loginConsentRequest.signature.signature
     );
 
