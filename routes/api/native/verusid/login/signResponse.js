dbeabb9d03e36c66b4da1dd839877e64071d2495
mcstoer <49734282+mcstoer@users.noreply.github.com>
2024-10-16 19:07:43 -0700

    Change the signing of the login consent response to use the signdata command.

--- routes/api/native/verusid/login/signResponse.js
+++ routes/api/native/verusid/login/signResponse.js
@@ -4,15 +4,6 @@ const {
   LOGIN_CONSENT_RESPONSE_SIG_VDXF_KEY,
 } = require("verus-typescript-primitives");
 
-const {
-  ECPair,
-  networks,
-  IdentitySignature
-} = require("@bitgo/utxo-lib");
-
-const ID_SIG_VERSION = 2
-const ID_SIG_TYPE = 5
-
 module.exports = (api) => {
   api.native.verusid.login.sign_response = async (response) => {
     const loginResponse = new LoginConsentResponse(response);
@@ -25,33 +16,15 @@ module.exports = (api) => {
       throw new Error(verificatonCheck.message);
     }
 
-    // Sign the decision hash of the response not using the daemon since sign_hash is disabled.
-    const info = await api.native.get_info(response.chain_id)
-
-    const signingIdentity = await api.native.get_identity(response.chain_id, loginResponse.signing_id);
-
-    const network = response.chain_id == "VRSC" ? networks.verus : networks.verustest;
-
-    const wif = await api.native.get_privkey(response.chain_id, signingIdentity.identity.primaryaddresses[0])
-
-    const keyPair = ECPair.fromWIF(wif, network);
-
-    const sig = new IdentitySignature(
-      network,
-      ID_SIG_VERSION,
-      ID_SIG_TYPE,
-      info.longestchain,
-      null,
-      response.system_id,
-      response.signing_id
-    );
-
-    sig.signHashOffline(loginResponse.getDecisionHash(info.longestchain), keyPair);
-
-    const signedSig = sig.toBuffer().toString("base64");
+    const signdataResult = await api.native.sign_data(response.chain_id,
+      {
+        "address": loginResponse.signing_id,
+        "datahash": loginResponse.decision.toSha256().toString("hex")
+      }
+    )
 
     loginResponse.signature = new VerusIDSignature(
-      { signature: signedSig },
+      { signature: signdataResult.signature },
       LOGIN_CONSENT_RESPONSE_SIG_VDXF_KEY
     );
 
