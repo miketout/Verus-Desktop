7569c1b9557eb04497720ddd38db395db18033f8
mcstoer <49734282+mcstoer@users.noreply.github.com>
2024-10-23 16:55:30 -0700

    Add api call for calling the daemon with the getblock command.

--- routes/api.js
+++ routes/api.js
@@ -130,6 +130,7 @@ api = require('./api/native/callDaemon')(api);
 api = require('./api/native/addresses')(api);
 api = require('./api/native/balances')(api);
 api = require('./api/native/definedchains')(api);
+api = require('./api/native/getBlock.js')(api);
 api = require('./api/native/info')(api);
 api = require('./api/native/mininginfo')(api);
 api = require('./api/native/getTransaction.js')(api);
