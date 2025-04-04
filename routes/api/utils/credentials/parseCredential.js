bffb56a3b38e465b9fdf5aec46e941aa5984af39
mcstoer <49734282+mcstoer@users.noreply.github.com>
2025-04-03 22:36:06 -0700

    Add credential decryption from an identity and fix formatting for the api of z_getencryptionaddress.

---
+++ routes/api/utils/credentials/parseCredential.js
@@ -0,0 +1,15 @@
+const {
+  Credential,
+  DATA_TYPE_OBJECT_CREDENTIAL,
+} = require("verus-typescript-primitives");
+
+// parseCredential converts a credential object from a data descriptor into a credential class.
+const parseCredential = (credObj) => {
+  const credJson = credObj[DATA_TYPE_OBJECT_CREDENTIAL.vdxfid];
+
+  return Credential.fromJSON(credJson);
+};
+
+module.exports = {
+  parseCredential
+};
