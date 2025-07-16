ae45c82a181bab3b641876670d1dc6053afe6823
mcstoer <49734282+mcstoer@users.noreply.github.com>
2025-07-16 13:37:26 -0700

    Add verification for identity update requests and refactor how verusid api functions are included in the api structure.

--- routes/api/native/verusid/verusid.js
+++ routes/api/native/verusid/verusid.js
@@ -2,6 +2,18 @@ module.exports = (api) => {
   api.native.verusid = {}
   api.native.verusid.login = {}
   api.native.verusid.provision = {}
+  api.native.verusid.identity = {}
+
+  // Identity
+  require('./identity/verifyIdentityUpdateRequest')(api);
+  
+  // Login
+  require('./login/verifyRequest')(api);
+  require('./login/signResponse')(api);
+  
+  // Provisioning
+  require('./provision/signIdProvisioningRequest')(api);
+  require('./provision/verifyIdProvisioningResponse')(api);
 
   return api;
 };
